2dafde4f6ac3896a4a8d8a11dea51a8b39bd84e3
Dylan Harville <dylanharville@Dylans-MacBook-Pro.local>
2022-08-21 19:54:40 -0700

    initial template for slack channel codes

---
+++ channellist.js
@@ -0,0 +1,18 @@
+//channel list
+const channels = [
+    BSC = [],
+    ETH = [],
+    MATIC = [],
+    SOL = [],
+    ALGO = [],
+    ARB = [],
+    AVAX = [],
+    BTC = [],
+    CELO = [],
+    FTM = [],
+    XDAI = [],
+    ONE = [],
+    OP = [],
+    PALM = [],
+    LUNA = []
+]
