034009993e3b2024981640dbe4b7b3c0dbd44505
D <dylan@quiknode.io>
2022-12-09 11:32:33 -0800

    file structure overhaul. pulled logic from task-app. functionallity needs to be customized.

---
+++ DB/ETH.js
@@ -0,0 +1,28 @@
+const AggregateNamesAndIds = require('../utils/aggregator');
+
+const ETH = [
+  {
+    name: "test",
+    id: "123",
+  },
+  {
+    name: "test1",
+    id: "234",
+  },
+  {
+    name: "test2",
+    id: "345",
+  },
+];
+
+let ethChannelIds = [];
+let ethChannelNames = [];
+
+AggregateNamesAndIds(ETH, ethChannelNames, ethChannelIds);
+console.log(ethChannelNames, ethChannelIds);
+
+module.exports = {
+  ethChannelNames, 
+  ethChannelIds, 
+  ETH
+};
